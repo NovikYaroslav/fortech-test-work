fc5f78f3674f90d4a647397d56f165de1b1bc4a9
Novik <yaroslavnowik@yandex.ru>
2024-01-05 18:19:23 +0100

    pokemon page responsive refactired

--- src/components/Not-found/Not-found.jsx
+++ src/components/Not-found/Not-found.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import psyduck from '../../img/psyduck.png';
 import './Not-found.css';
 
 export default function NotFound() {
+  const currentLocation = useLocation();
   return (
     <div className="not-found">
       <img className="not-found__image" src={psyduck} alt="psyduck" />
       <p className="not-found__text"> &rdquo;OH, OH, OH NOTHING FOUND&rdquo;</p>
+      {currentLocation.pathname.includes('pokemon')
+        ? <p className="not-found__text"> &rdquo;PLEASE PROCEED BACK&rdquo;</p>
+        : <p className="not-found__text"> &rdquo;TRY CHANGING YOU SEARCH PARAMENETERS&rdquo;</p>}
+
     </div>
   );
 }
